d6bc1b1e75b8945396bce1612f771fe149259bf3
dazzlemon <daanill13470@gmail.com>
2022-10-30 12:52:36 +0200

    main3

--- 2/scripts/compile.sh
+++ 2/scripts/compile.sh
@@ -1,2 +1,3 @@
 g++ src/main1.cpp -o bin/main1
 g++ src/main2.cpp -o bin/main2
+g++ src/main3.cpp -o bin/main3
--- 2/scripts/disassemble.sh
+++ 2/scripts/disassemble.sh
@@ -1,2 +1,3 @@
 objdump --disassemble-all bin/main2 > main2.disassembled.txt
 objdump --disassemble-all bin/main2.cracked > main2.cracked.disassembled.txt
+objdump --disassemble-all bin/main3 > main3.disassembled.txt
