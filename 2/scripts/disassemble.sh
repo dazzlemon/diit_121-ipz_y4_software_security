78f146e75f003201b1d04b79419bf460e59c071f
dazzlemon <daanill13470@gmail.com>
2022-10-30 13:14:43 +0200

    demangle

--- 2/scripts/disassemble.sh
+++ 2/scripts/disassemble.sh
@@ -1,3 +1,4 @@
 objdump --disassemble-all bin/main2 > main2.disassembled.txt
 objdump --disassemble-all bin/main2.cracked > main2.cracked.disassembled.txt
 objdump --disassemble-all bin/main3 > main3.disassembled.txt
+objdump --disassemble-all bin/main3 > main3.disassembled.demangled.txt -C
